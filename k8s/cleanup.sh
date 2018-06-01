9ccc3da84aae5265caf7e803b9ab2577f6696677
Patrick Lang <plang@microsoft.com>
2018-05-31 22:46:40 -0700

    Fixing cleanup script

--- k8s/cleanup.sh
+++ k8s/cleanup.sh
@@ -1,4 +1,16 @@
-kubectl delete -n plang1 secret mssql
-kubectl delete -n plang1 route --all
-kubectl delete -n plang1 deploy --all
-kubectl delete -n plang1 svc --all
+#!/bin/bash
+
+namespace_opt=""
+
+while getopts "n:" opt; do
+    case "$opt" in
+    n) namespace_opt="-n $OPTARG"
+       ;;
+    esac
+done
+
+kubectl delete ${namespace_opt} secret mssql
+kubectl delete ${namespace_opt} deployment fabrikamfiber.web 
+kubectl delete ${namespace_opt} deployment db 
+kubectl delete ${namespace_opt} svc fabrikamfiberweb 
+kubectl delete ${namespace_opt} svc db 
